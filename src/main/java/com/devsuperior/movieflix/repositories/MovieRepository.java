5cedb60aa83cf13ed71fcf34de377ae12fad2348
Andre Proenca <andreproenca@hotmail.com>
2025-11-17 16:50:42 -0400

    testes MovieControllerIT passed

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,5 +1,8 @@
 package com.devsuperior.movieflix.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +11,10 @@ import com.devsuperior.movieflix.entities.Movie;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 	
+	@EntityGraph(attributePaths = "genre")
+	Page<Movie> findAll(Pageable pageable);
 	
-	
+	@EntityGraph(attributePaths = "genre")
+	Page<Movie> findByGenreId(Long genreId, Pageable pageable);	
 
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -3,8 +3,14 @@ package com.devsuperior.movieflix.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.MovieRepository;
@@ -20,6 +26,26 @@ public class MovieService {
 		Optional<Movie> obj = repository.findById(id);
 		Movie entity = obj.orElseThrow(() -> new  ResourceNotFoundException("Entity not found"));
 		return new MovieDetailsDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<MovieCardDTO> findAllPaged(Long genreId, Pageable pageable) {
+		
+		Pageable sorted = PageRequest.of(
+				pageable.getPageNumber(),
+				pageable.getPageSize(),
+				Sort.by("title")
+		);
+		
+		Page<Movie> page;
+		
+		if(genreId == null || genreId == 0) {
+			page = repository.findAll(sorted);
+		} else {
+			page = repository.findByGenreId(genreId, pageable);
+		}
+		
+		return page.map(MovieCardDTO::new);
 	}
 
 }
